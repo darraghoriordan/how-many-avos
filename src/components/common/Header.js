0d75c9a30c4891eedbe0a98af6a7634e8ac21038
Darragh ORiordan <darragh.oriordan@gmail.com>
2017-04-29 15:13:39 +1200

    add bootstrap navigation

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,19 +1,36 @@
 import React, {PropTypes} from 'react';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import LoadingDots from './LoadingDots';
+import {Navbar, Nav, NavItem} from 'react-bootstrap';
+import {LinkContainer, IndexLinkContainer} from 'react-router-bootstrap';
 
 const Header = ({loading}) => {
   return (
-    <nav>
-                   <NavLink to="/" activeClassName="active">Home</NavLink>
-                {" | "}
-                <NavLink to="/courses">courses list</NavLink>
-                {" | "}
-                <NavLink to="/about">about</NavLink>
-                {" | "}
-                <NavLink to="/howmanyavos">How many avos</NavLink>
-      {loading && <LoadingDots interval={100} dots={20}/>}
-    </nav>
+    <Navbar collapseOnSelect>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <NavLink to="/">Avo Calc</NavLink>
+        </Navbar.Brand>
+        <Navbar.Toggle/>
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <IndexLinkContainer to="/" className="nav-item">
+            <NavItem eventKey={1}>Home</NavItem>
+          </IndexLinkContainer>
+          <LinkContainer to="/about" activeClassName="active" className="nav-item">
+            <NavItem eventKey={2}>About</NavItem>
+          </LinkContainer>
+          <LinkContainer to="/howmanyavos" activeClassName="active" className="nav-item">
+            <NavItem eventKey={3}>Avo Calc</NavItem>
+          </LinkContainer>
+        </Nav>
+        <Navbar.Text>
+          {loading && <LoadingDots interval={100} dots={20}/>}
+        </Navbar.Text>
+        <Nav pullRight></Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
