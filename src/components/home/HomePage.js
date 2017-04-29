b1200b13146e47e75e9cccf5b294a209d4b573c3
Darragh ORiordan <darragh.oriordan@gmail.com>
2017-04-29 16:01:43 +1200

    add a home page thing

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import {Jumbotron} from 'react-bootstrap';
 
 class HomePage extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Home</h1>
-                <p>Home home?</p>
 
-            </div>
+            <Jumbotron>
+                <h1>Hello,  millennial!</h1>
+                <p>This site will help you on your journey to the Kiwi dream of home ownership. It's going to take 
+                    some effort from you so get ready to have your last latte!
+                </p>
+            </Jumbotron>
         );
     }
 }
