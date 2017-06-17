10ed8eb5a74acd264a4295f1b9e126512a254f2d
Darragh ORiordan <darragh.oriordan@gmail.com>
2017-06-17 23:20:36 +1200

    add some wrappers

--- src/components/avoCalculator/AvoCalculatorResult.js
+++ src/components/avoCalculator/AvoCalculatorResult.js
@@ -4,25 +4,77 @@ import {Table} from 'react-bootstrap';
 const AvoCalculatorResult = ({avoCalculatorResults}) => {
 
     return (
-        <div>
+        <div className="avocalculator-results">
+            <p className="avocalculator-results__header">
+                Calculate your order here:</p>
+
             <p>
-                Damn, you need a deposit of {avoCalculatorResults.deposit.toLocaleString("en-NZ", {style: "currency", currency: "NZD", minimumFractionDigits: 2})}
+                damn, you need a deposit of:
+                <span className="avocalculator-results__value">
+                    {avoCalculatorResults
+                        .deposit
+                        .toLocaleString("en-NZ", {
+                            style: "currency",
+                            currency: "NZD",
+                            minimumFractionDigits: 2
+                        })}</span>
+            </p>
+            <hr className="avocalculator-results__seperator"/>
+            <p>
+                with your #$*^%ing huge gift of
+                <span className="avocalculator-results__value">{avoCalculatorResults
+                        .giftFromParents
+                        .amount
+                        .toLocaleString("en-NZ", {
+                            style: "currency",
+                            currency: "NZD",
+                            minimumFractionDigits: 2
+                        })}</span>
             </p>
             <p>
-                with your fucking huge gift of {avoCalculatorResults.giftFromParents.amount.toLocaleString("en-NZ", {style: "currency", currency: "NZD", minimumFractionDigits: 2})}
+                and
+                <span className="avocalculator-results__value">{avoCalculatorResults
+                        .personalSavings
+                        .weeklySavings
+                        .toLocaleString("en-NZ", {
+                            style: "currency",
+                            currency: "NZD",
+                            minimumFractionDigits: 2
+                        })}</span>&nbsp;weekly savings
             </p>
             <p>
-                and {avoCalculatorResults.personalSavings.weeklySavings.toLocaleString("en-NZ", {style: "currency", currency: "NZD", minimumFractionDigits: 2})}&nbsp;weekly savings
+                and saving
+                <span className="avocalculator-results__value">{avoCalculatorResults
+                        .lattes
+                        .weeklySavings
+                        .toLocaleString("en-NZ", {
+                            style: "currency",
+                            currency: "NZD",
+                            minimumFractionDigits: 2
+                        })}</span>&nbsp;on lattes
             </p>
             <p>
-                and saving {avoCalculatorResults.lattes.weeklySavings.toLocaleString("en-NZ", {style: "currency", currency: "NZD", minimumFractionDigits: 2})}&nbsp;on lattes 
+                and
+                <span className="avocalculator-results__value">{avoCalculatorResults
+                        .avoBreakfasts
+                        .weeklySavings
+                        .toLocaleString("en-NZ", {
+                            style: "currency",
+                            currency: "NZD",
+                            minimumFractionDigits: 2
+                        })}</span>&nbsp;on avo brekkies
             </p>
             <p>
-                and {avoCalculatorResults.avoBreakfasts.weeklySavings.toLocaleString("en-NZ", {style: "currency", currency: "NZD", minimumFractionDigits: 2})}&nbsp;on avo brekkies 
+                and total weekly savings of
+                <span className="avocalculator-results__value">{avoCalculatorResults
+                        .totalWeeklySavings
+                        .toLocaleString("en-NZ", {
+                            style: "currency",
+                            currency: "NZD",
+                            minimumFractionDigits: 2
+                        })}</span>
             </p>
-            <p>
-                Your total weekly savings will be {avoCalculatorResults.totalWeeklySavings.toLocaleString("en-NZ", {style: "currency", currency: "NZD", minimumFractionDigits: 2})}
-            </p>
+            <hr className="avocalculator-results__seperator"/>
             <p>
                 You can save your deposit in {avoCalculatorResults
                     .numberOfWeeksToDeposit
@@ -31,11 +83,24 @@ const AvoCalculatorResult = ({avoCalculatorResults}) => {
                     .toFixed(2)}&nbsp;years)
             </p>
             <p>
-           Your mortgage payments will be {avoCalculatorResults.monthlyMortgagePayment.toLocaleString("en-NZ", {style: "currency", currency: "NZD", minimumFractionDigits: 2})} per month
+                Your mortgage payments will be
+                <span className="avocalculator-results__value">{avoCalculatorResults
+                        .monthlyMortgagePayment
+                        .toLocaleString("en-NZ", {
+                            style: "currency",
+                            currency: "NZD",
+                            minimumFractionDigits: 2
+                        })}</span>
+                per month
             </p>
+            <hr className="avocalculator-results__seperator"/>
             <p>
-                  This house will really cost you {(avoCalculatorResults.monthlyMortgagePayment * 12 * 30).toLocaleString("en-NZ", {style: "currency", currency: "NZD", minimumFractionDigits: 2})}
-                </p>
+                This house will really cost you {(avoCalculatorResults.monthlyMortgagePayment * 12 * 30).toLocaleString("en-NZ", {
+                    style: "currency",
+                    currency: "NZD",
+                    minimumFractionDigits: 2
+                })}
+            </p>
             <Table striped bordered condensed hover>
                 <thead>
                     <tr>
